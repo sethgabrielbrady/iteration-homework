f2de2ce6a98eca845a79dcd5c196b966827cbc8e
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-02-28 17:04:41 -0500

    sonnets properly functioning

--- moSeth.js
+++ moSeth.js
@@ -1,65 +1,30 @@
-let sonnets = require('./sonnets.js')
+let sonnets = require('./sonnets.js');
 
 
 
-function wordFrequency(inputString){//creates a function that takes a string as an argument
+function wordFrequency(inputString){
     let splitString = inputString.split(' ');
-      console.log(splitString);//show that out string gets put into the array splitString
-    let stringContainer = new Object();
-          //for each index of array splitString, execute function propertyAdd, taking the value of
-          //each index of splitString as an argument.
+    let stringContainer = {};
+
     splitString.forEach(function propertyAdd(each) {
         if (stringContainer[each] >= 1){
-            stringContainer[each]= stringContainer[each] + 1;
+            stringContainer[each] = stringContainer[each] + 1;
           }else {
               stringContainer[each] = 1;
               }
-          console.log(stringContainer);//testing
         });
+
+        return stringContainer;
+
 }
 
-wordFrequency("The cat in the hat cat the");
+
+wordFrequency(sonnets);
 
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let testRunner = require('./word-frequency-tests.js');
-// testRunner(wordFrequency);
+let testRunner = require('./word-frequency-tests.js');
+testRunner(wordFrequency);
