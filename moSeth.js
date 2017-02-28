e216050cbd582d5328d81d4a908ed6d5906a6e38
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-02-28 15:42:58 -0500

    used 4each to add properties inside our object container

--- moSeth.js
+++ moSeth.js
@@ -1,11 +1,17 @@
-let sonnets = require('./sonnets.js');
+let sonnets = require('./sonnets.js')
 
 
 
-function wordFrequency(inputString){
-
+function wordFrequency(inputString){//creates a function that takes a string as an argument
     let splitString = inputString.split(' ');
-    console.log(splitString);
+      console.log(splitString);//show that out string gets put into the array splitString
+    let stringContainer = new Object();
+          //for each index of array splitString, execute function propertyAdd, taking the value of
+          //each index of splitString as an argument.
+    splitString.forEach(function propertyAdd(each) {
+          stringContainer[each] = 1;
+          console.log(stringContainer);//testing
+    });
 
 }
 
@@ -52,5 +58,5 @@ wordFrequency("The cat in the hat cat the");
 
 
 
-let testRunner = require('./word-frequency-tests.js');
-testRunner(wordFrequency);
+// let testRunner = require('./word-frequency-tests.js');
+// testRunner(wordFrequency);
